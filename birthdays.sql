c80f20c85ac96378e0652b248bae6291a44f640f
GrantAccess <97744168+GrantAccess@users.noreply.github.com>
2022-02-22 17:26:03 +0000

    Spell check

--- birthdays.sql
+++ birthdays.sql
@@ -1,14 +1,14 @@
 /*
 Routine of birthdays.sql
-a script to make a view table, looking at two other tables with forign key relationship
+a script to make a view table, looking at two other tables with foreign key relationship
 by <https://github.com/GrantAccess/castle-vagabond/> (CC)
 the author has not changed this document since Tuesday 22 Feb 2022
 */
 
 /*
 There are two tables
-The people table holds columbs of unique info and a forign key 'first_name'
- which contains the same number, as the primary key, to the list of names table  
+The people table holds columns of unique info and a foreign key 'first_name'
+ which contains the same number, as the primary key, to the list of names table
 The fst_name holds a list of first names and its primary key 'fst_name_id'
 */
 
